483dd6c3f71d17719388a0380320519107f476f7
Iqra-Noor5 <piqra3467@gmail.com>
2026-08-29 16:49:46 +0500

    Task 1

---
+++ 26p-3010-task 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+int batchyear=2026;        
+char sect='A';
+float ent_sc=70.2;
+double sem_fee=216000.00;
+printf("========================================================\n");
+printf("\t\t\tFAST-NUCES STUDENT PROFILE CARD\n");
+printf("========================================================\n\n");
+printf("\tBatch year\t:\t%d\n",batchyear);
+printf("\tSection\t\t:\t%c\n",sect);
+printf("\tTest Score\t:\t%.1f\n",ent_sc);
+printf("\tSemester Fee :\t%.2f\n",sem_fee);
+printf("\tMotto\t \t:\tSmall daily steps lead to big success\n");
+printf("========================================================\n");
+    return 0;
+}
