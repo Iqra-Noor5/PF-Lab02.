0a55668cd09f465a7b3a1fdeb3abe54bde101b4f
Iqra-Noor5 <piqra3467@gmail.com>
2026-08-30 13:31:02 +0500

    Add files via upload

---
+++ 26p-3010-task 4.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+int main() 
+{
+printf("============== C ESCAPE SEQUENCES REFERENCE ==============\n");
+    printf("1. To print a double quote, write:\t\\\"\n");
+    printf("2. To print a literal backslash, write:\t\\\\\n");
+    printf("3. To insert a newline break, write:\t\\n\n");
+    printf("4. To insert a horizontal tab stop, write:\t\\t\n");
+    printf("5. To print a percent sign, write:\t%%%%\n");
+    printf("==========================================================\n");
+    
+    return 0;
+}
