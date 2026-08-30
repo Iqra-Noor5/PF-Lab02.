75310d1275507781f5bde9691445f039ae13d8b6
Iqra-Noor5 <piqra3467@gmail.com>
2026-08-30 13:17:55 +0500

    26p-3010-task2.c

--- 26p-3010-task 2.c
+++ 26p-3010-task 2.c
@@ -12,7 +12,7 @@ printf("\tBatch year\t:\t%d\n",batchyear);
 printf("\tSection\t\t:\t%c\n",sect);
 printf("\tTest Score\t:\t%.1f\n",ent_sc);
 printf("\tSemester Fee :\t%.2f\n",sem_fee);
-printf("\tMotto\t \t:\tSmall daily steps lead to big success\n");
+printf("\tMotto\t \t:\t\"Small daily steps lead to big success\"\n");
 printf("========================================================\n");
     return 0;
 }
